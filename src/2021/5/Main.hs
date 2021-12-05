df707578456fcbc0d60c90c8ab3253ba35f8522e
ShaharR <shaharr@wix.com>
2021-12-05 08:31:15 +0200

    refactor

--- src/2021/5/Main.hs
+++ src/2021/5/Main.hs
@@ -5,63 +5,65 @@ import Control.Monad (guard)
 import Debug.Trace ( traceShow )
 
 type Point = (Int, Int)
+type Edges = (Point, Point)
 type Line = [Point]
-
-getFromTo :: String -> (Point, Point)
-getFromTo st = (p1, p2)
-    where
-        [fromSt, toSt] = splitOn " -> " st
-        [fromX, fromY] = map read $ splitOn "," fromSt
-        [toX, toY] = map read $ splitOn "," toSt
-        p1 = (fromX, fromY)
-        p2 = (toX, toY)
-
-readDiagLine :: (Point, Point) -> [Point]
-readDiagLine (p1, p2) = line
-    where
-        (fromX, fromY) = p1
-        (toX, toY) = p2
-        xs = if fromX > toX then reverse [toX..fromX] else [fromX..toX]
-        ys = if fromY > toY then reverse [toY..fromY] else [fromY..toY]
-        line = zip xs ys
-
-        
-
-readLine :: (Point, Point) -> [Point]
-readLine (p1, p2) = line
-    where
-        (fromX, fromY) = p1
-        (toX, toY) = p2
-        xs = if fromX > toX then [toX..fromX] else [fromX..toX]
-        ys = if fromY > toY then [toY..fromY] else [fromY..toY]
-        line = [ (x, y) | x <- xs, y <- ys ]
 
 readInputFile :: IO String
 readInputFile = readFile "./input.txt"
 
-part1 :: [[Point]] -> Int
-part1 inp = 5
+readCommaDelimitedNums :: String -> [Int]
+readCommaDelimitedNums = map read . splitOn ","
 
-part2 :: [[Point]] -> Int
-part2 inp = 5
+getEdges :: String -> Edges
+getEdges st = ((fromX, fromY), (toX, toY))
+    where
+        [fromSt, toSt] = splitOn " -> " st
+        [fromX, fromY] = readCommaDelimitedNums fromSt
+        [toX, toY] = readCommaDelimitedNums toSt
 
-isHorOrVer :: (Point, Point) -> Bool
-isHorOrVer ((xFrom, yFrom), (xTo, yTo)) = xFrom == xTo || yFrom == yTo
+getRange :: Edges -> ([Int], [Int])
+getRange ((fromX, fromY), (toX, toY)) = (
+    if fromX > toX then reverse [toX..fromX] else [fromX..toX],
+    if fromY > toY then reverse [toY..fromY] else [fromY..toY]
+    )
 
-isDiag :: (Point, Point) -> Bool
-isDiag ((xFrom, yFrom), (xTo, yTo)) = abs (xFrom - xTo) == abs (yFrom - yTo)
+getDiagonalLine :: Edges -> Line
+getDiagonalLine edges = line
+    where
+        (xs, ys) = getRange edges
+        line = zip xs ys
+
+getHorizontalVerticalLine :: Edges -> Line
+getHorizontalVerticalLine edges = line
+    where
+        (xs, ys) = getRange edges
+        line = [ (x, y) | x <- xs, y <- ys ]
+
+isHorizontalVertical :: Edges -> Bool
+isHorizontalVertical ((xFrom, yFrom), (xTo, yTo)) = xFrom == xTo || yFrom == yTo
+
+isDiagonal :: Edges -> Bool
+isDiagonal ((xFrom, yFrom), (xTo, yTo)) = abs (xFrom - xTo) == abs (yFrom - yTo)
+
+getHorizontalPoints :: [Edges] -> Line
+getHorizontalPoints = concatMap getHorizontalVerticalLine . filter isHorizontalVertical
+
+getDiagonalPoints :: [Edges] -> Line
+getDiagonalPoints = concatMap getDiagonalLine . filter isDiagonal
+
+countRepeating :: (Ord a) => Int -> [a] -> Int
+countRepeating moreThan = length . filter ((>=moreThan) . length) . group . sort
+
+part1 :: [Edges] -> Int
+part1 inp = countRepeating 2 (getHorizontalPoints inp)
+
+part2 :: [Edges] -> Int
+part2 inp = countRepeating 2 (getDiagonalPoints inp ++ getHorizontalPoints inp)
 
 main :: IO ()
 main = do
     contents <- readInputFile
-    let lns = lines contents
-    let horVerFromTos = filter isHorOrVer (map getFromTo lns)
-    let diagLines = filter isDiag (map getFromTo lns)
-    -- print diagLines
-    let lines = map readLine horVerFromTos
-    let linesDiag = map readDiagLine diagLines
-    let all = lines ++ linesDiag
-    print $ length $ filter (\gr -> length gr >= 2) $ group $ sort (concat all)
+    let edges = map getEdges $ lines contents
 
-    -- print $ part1 lines
-    -- print $ part2 lines
+    print $ part1 edges
+    print $ part2 edges
