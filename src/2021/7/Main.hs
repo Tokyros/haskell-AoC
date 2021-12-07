e1653c08256bc89df4ff63e5a211094e6b557e86
ShaharR <shaharr@wix.com>
2021-12-07 07:18:34 +0200

    day 7 - pre-refactor

---
+++ src/2021/7/Main.hs
@@ -0,0 +1,43 @@
+module Main where
+import Data.List
+import Data.List.Split ( splitOn )
+import Debug.Trace (traceShow)
+
+readInputFile :: IO Inp
+readInputFile = do
+    contents <- readFile "./input.txt"
+    return $ parseInp contents
+
+parseInp :: String -> Inp
+parseInp str = inp
+    where
+        inp = map read (splitOn  "," str)
+
+type Inp = [Int]
+
+fac :: (Integral a) => a -> a
+fac n = sum [1..n]
+
+part1 :: Inp -> Int
+part1 inp = res
+    where 
+        fuels = map (\p -> sum $ map (\i -> abs (i - p)) inp) inp
+        min = minimum fuels
+        res = min
+
+part2 :: Inp -> Int
+part2 inp = res
+    where 
+        minInp = minimum inp
+        maxInp = maximum inp
+        adjInp = [minInp..maxInp]
+        fuels = map (\p -> sum $ map (\i -> sum [1..(abs (i-p))]) inp) adjInp
+        min = minimum fuels
+        res = min
+
+main :: IO ()
+main = do
+    inp <- readInputFile
+
+    -- print $ part1 inp
+    print $ part2 inp
