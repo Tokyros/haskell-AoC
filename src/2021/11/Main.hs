c09dc57e03b3a3b5d82b9ad9491a768a8a27de83
ShaharR <shaharr@wix.com>
2021-12-11 13:20:28 +0200

    refactor day 11

--- src/2021/11/Main.hs
+++ src/2021/11/Main.hs
@@ -6,6 +6,7 @@ import Debug.Trace (traceShow, traceShowId)
 import qualified Data.Set as Set
 import Data.Maybe (fromJust, catMaybes, mapMaybe, isNothing)
 import qualified Data.Bifunctor
+import qualified Data.Map as M
 
 
 showDumbo :: Dumbo -> String
@@ -13,22 +14,25 @@ showDumbo (Charging i) = show i
 showDumbo JustFlashed = "f"
 showDumbo Flashed = "F"
 
-showGrid :: (Int, Inp) -> IO ()
+showGrid :: (Int, Grid Dumbo) -> IO ()
 showGrid (i, g) = do
-    putStrLn $ foldl (\acc ((r, c), d) -> acc ++ (if c == 1 then "\n" else "") ++ showDumbo d ++ " ") "" g
+    putStrLn $ M.foldlWithKey (\acc (r, c) d -> acc ++ (if c == 1 then "\n" else "") ++ showDumbo d ++ " ") "" g
     putStrLn $ "\n" ++ show i
+
+type Grid a = M.Map (Int, Int) a
 
 readInputFile :: IO Inp
 readInputFile = do
     contents <- readFile "./input.txt"
     return $ parseInp contents
 
-mapGridToPoint :: [[a]] -> [(Coordinate, a)]
-mapGridToPoint g = points
+mapGridToPoint :: [[a]] -> Grid a
+mapGridToPoint g = pointsGrid
     where
         indexedCols = map (zip [1..]) g
         indexedRows = zip [1..] indexedCols
         points = concatMap (\(rowIdx, row) -> map (\(colIdx, a) -> ((rowIdx, colIdx), a)) row) indexedRows
+        pointsGrid = M.fromList points
 
 parseInp :: String -> Inp
 parseInp str = inp
@@ -41,74 +45,75 @@ parseInp str = inp
 data Dumbo = Charging Int | Flashed | JustFlashed deriving (Show)
 
 type Coordinate = (Int, Int)
-type Point = (Coordinate, Dumbo)
-type Inp = [Point]
+type Inp = Grid Dumbo
+type StepResult = (Inp, Int)
 
-isAdjacent :: Coordinate -> Coordinate -> Bool
-isAdjacent (r1, c1) (r2, c2) = (r1, c1) /= (r2, c2) && abs (r1 - r2) <= 1 && abs (c1 - c2) <= 1
+directions :: [(Int, Int)]
+directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
-getNeighbors :: Inp -> Point -> [Point]
-getNeighbors grid (p1, _) = filter (\(p2, _) -> isAdjacent p1 p2) grid
+getNeighbors :: Inp -> Coordinate -> Inp
+getNeighbors grid (r, c) = M.filterWithKey (\p2 _ -> p2 `elem` neighborCoords) grid
+    where neighborCoords = map (Data.Bifunctor.bimap (r +) (c +)) directions
 
 justFlashed :: Dumbo -> Bool
 justFlashed JustFlashed = True
 justFlashed _ = False
 
-getNeighborsJustFlashed :: Inp -> Point -> [Point]
-getNeighborsJustFlashed inp p = filter (justFlashed . snd) $ getNeighbors inp p
+getNeighborsJustFlashedCount :: Inp -> Coordinate -> Int
+getNeighborsJustFlashedCount inp p = length $ M.filter justFlashed $ getNeighbors inp p
 
-getNeighborsJustFlashedCount :: Inp -> Point -> Int
-getNeighborsJustFlashedCount inp p = length $ getNeighborsJustFlashed inp p
-
-increaseByOne :: Point -> Point
-increaseByOne (p, Charging i) = (p, Charging (i+1))
+increaseByOne :: Dumbo -> Dumbo
+increaseByOne (Charging i) = Charging (i+1)
 increaseByOne n = undefined
 
-maybeFlash :: Point -> Point
-maybeFlash (p, Charging i) = if i > 9 then (p, JustFlashed) else (p, Charging i)
+maybeFlash :: Dumbo -> Dumbo
+maybeFlash (Charging i) = if i > 9 then JustFlashed else Charging i
 maybeFlash n = n
 
-increaseDumbo :: Inp -> Point -> Point
-increaseDumbo grid p@((row, col), Charging i) = ((row, col), Charging (i + getNeighborsJustFlashedCount grid p))
-increaseDumbo grid ((row, col), JustFlashed) = ((row, col), Flashed)
-increaseDumbo grid ((row, col), Flashed) = ((row, col), Flashed)
+increaseDumbo :: Int -> Dumbo -> Dumbo
+increaseDumbo by (Charging i) = Charging (i + by)
+increaseDumbo _ JustFlashed = Flashed
+increaseDumbo _ Flashed = Flashed
 
 resetDumbo :: Dumbo -> Dumbo
 resetDumbo Flashed = Charging 0
 resetDumbo n = n
 
 resetGrid :: Inp -> Inp
-resetGrid = map (Data.Bifunctor.second resetDumbo)
+resetGrid = M.map resetDumbo
 
-step :: Inp -> (Int, Inp)
+step :: Inp -> StepResult
 step dumbos = res
     where
-        increasedDumbo = map increaseByOne dumbos
-        res = step' (0, increasedDumbo)
+        increasedDumbo = M.map increaseByOne dumbos
+        res = step' (increasedDumbo, 0)
 
-step' :: (Int, Inp) -> (Int, Inp)
-step' (acc, dumbos) = res
+mapAny :: (a -> Bool) -> M.Map b a -> Bool
+mapAny pred m = (>0) $ M.size (M.filter pred m)
+
+step' :: StepResult -> StepResult
+step' (dumbos, flashedAcc)
+    | hasAnyJustFlashed = step' (next, flashedAcc + countFlashed)
+    | otherwise = (resetGrid maybeFlashedDumbos, flashedAcc)
     where
-        maybeFlashedDumbos = map maybeFlash dumbos
-        hasAnyJustFlashed = any (justFlashed . snd) maybeFlashedDumbos
-        next = map (increaseDumbo maybeFlashedDumbos) maybeFlashedDumbos
-        countFlashed = length $ filter (justFlashed . snd) maybeFlashedDumbos
-        res = if hasAnyJustFlashed then step' (acc + countFlashed, next) else (acc, resetGrid maybeFlashedDumbos)
+        maybeFlashedDumbos = M.map maybeFlash dumbos
+        hasAnyJustFlashed = mapAny justFlashed maybeFlashedDumbos
+        next = M.mapWithKey (increaseDumbo . getNeighborsJustFlashedCount maybeFlashedDumbos) maybeFlashedDumbos
+        countFlashed = length $ M.filter justFlashed maybeFlashedDumbos
 
 part1 :: Inp -> Int
 part1 inp = res
     where
-        res = fst $ foldl (\(acc, g) _ -> let (c, newG) = step g in (acc + c, newG)) (0, inp) [1..100]
+        res = snd $ foldl (\(g, acc) _ -> let (newG, c) = step g in (newG, acc + c)) (inp, 0) [1..100]
 
 fst3 :: (a, b, c) -> a
 fst3 (a, _, _) = a
 
 part2 :: Inp -> Int
-part2 inp = res
+part2 inp = let (idx, _, _) = last stepShtut in idx + 1
     where
-        something = scanl (\(_, _, g) i -> let (c, newG) = step g in (i, c, newG)) (0, 0, inp) [1..]
-        stepShtut = takeWhile (\(_, acc, _) -> acc < 100) something
-        res = fst3 (last stepShtut) + 1
+        steps = scanl (\(_, g, _) i -> let (newG, c) = step g in (i, newG, c)) (0, inp, 0) [1..]
+        stepShtut = takeWhile (\(_, _, flashed) -> flashed < 100) steps
 
 main :: IO ()
 main = do
